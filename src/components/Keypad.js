f2a02299cb9d66341485e5bb77273dee1b6aa241
Rianna Cleary <rianna.cleary@flatironschool.com>
2020-10-08 14:48:21 +0000

    Done.

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,22 @@
 // Code Keypad Component Here
+import React, { Component } from 'react';
+
+class Keypad extends Component {
+
+  handleIsKeyUp = () => {
+    console.log('Entering password...')
+  }
+
+  render() {
+    return (
+      <div>
+       <input type='password' onKeyUp={this.handleIsKeyUp}/>
+      </div>
+    );
+  }
+}
+
+
+
+
+export default Keypad;
