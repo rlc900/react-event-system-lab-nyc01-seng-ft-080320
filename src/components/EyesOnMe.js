41481810605a6a752738849e66924b4bfe5885cd
Rianna Cleary <rianna.cleary@flatironschool.com>
2020-10-08 14:45:58 +0000

    Automatically backed up by Learn

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,24 @@
 // Code EyesOnMe Component Here
+import React, { Component } from 'react';
+
+class EyesOnMe extends Component {
+
+  handleFocus = () => {
+    console.log('Good!')
+  }
+
+  handleBlur = () => {
+    console.log('Hey! Eyes on me!')
+  }
+
+  render() {
+    return (
+      <div>
+        <button onBlur={this.handleBlur} onFocus={this.handleFocus} >Click me!</button>
+      </div>
+    );
+  }
+
+}
+
+export default EyesOnMe;
